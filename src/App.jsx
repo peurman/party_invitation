7effde21311ca150c5f0923c1e5d67d6d6a5644e
Esteban Manrupe <esteban_manrupe@hotmail.com>
2022-08-25 09:55:40 -0300

    various initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useRoutes } from "react-router-dom";
+import Card from "./component/Card";
+import "./index.css";
+
+function App() {
+  let element = useRoutes([
+    {
+      path: "/",
+      element: <Card />,
+    },
+  ]);
+
+  return element;
+}
+
+export default App;
---
+++ src/component/Card.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import "./Card.css";
+import $ from "jquery";
+
+var button1 = false;
+var button2 = false;
+var button3 = false;
+
+class Card extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    this.handleClick();
+  }
+
+  handleClick() {
+    $(".button1").on("click", function () {
+      button1 = !button1;
+      $(this).parent().toggleClass("expand");
+      if (button2) {
+        $(".button2").parent().toggleClass("expand");
+        button2 = false;
+      }
+      if (button3) {
+        $(".button3").parent().toggleClass("expand");
+        button3 = false;
+      }
+    });
+    $(".button2").on("click", function () {
+      $(this).parent().toggleClass("expand");
+      button2 = !button2;
+      if (button1) {
+        $(".button1").parent().toggleClass("expand");
+        button1 = false;
+      }
+      if (button3) {
+        $(".button3").parent().toggleClass("expand");
+        button3 = false;
+      }
+    });
+    $(".button3").on("click", function () {
+      $(this).parent().toggleClass("expand");
+      button3 = !button3;
+      if (button1) {
+        $(".button1").parent().toggleClass("expand");
+        button1 = false;
+      }
+      if (button2) {
+        $(".button2").parent().toggleClass("expand");
+        button2 = false;
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="container0">
+        <div className="cuando">¿CUÁNDO?</div>
+        <div className="container1">
+          <span className="button1" /* onClick={this.handleClick}*/>
+            &#10006;
+          </span>
+          <div className="content">
+            <div className="head">
+              <h1>¿cuándo?</h1>
+              {/* <p className="sub">
+              by
+              <a href="https://codepen.io/woodwork/" target="_blank">
+                Joe Harry
+              </a>
+            </p> */}
+            </div>
+            <div className="body">
+              <p>
+                <em>SABADO 24/9</em>
+                <br />
+                de 22 a 6 horas
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="donde">¿DÓNDE?</div>
+        <div className="container2">
+          <span className="button2">&#10006;</span>
+          <div className="content">
+            <div className="head">
+              <h1>¿dónde?</h1>
+              {/* <p className="sub">
+              by
+              <a href="https://codepen.io/woodwork/" target="_blank">
+                Joe Harry
+              </a>
+            </p> */}
+            </div>
+            <div className="body">
+              <p>
+                <em>Balneario MARBELLA</em>
+                <br />
+                Av. de los Trabajadores 4350
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="venis">¿VENÍS?</div>
+        <div className="container3">
+          <span className="button3">&#10006;</span>
+          <div className="content3">
+            <div className="head">
+              <h1>¿venís?</h1>
+              {/* <p className="sub">
+              by
+              <a href="https://codepen.io/woodwork/" target="_blank">
+                Joe Harry
+              </a>
+            </p> */}
+            </div>
+            <div className="body">
+              <p>
+                Porfa confirmame tu asistencia <em>por SI o por NO</em> en mi
+                WhatsApp:
+              </p>
+              <div className="action-bar">
+                <a href="https://web.whatsapp.com/">WHATSAPP</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
